5c09daf998aca2625dc3c615842c9075784e8e99
francojnieva <franco97nieva@hotmail.com>
2023-10-25 17:44:29 -0300

    :art: add video to sections 1,3. add functionality to the questions section icons and add login.html

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,22 @@
 const btnAccordion = document.querySelectorAll(".btn-accordion")
 const answerAccordion = document.querySelectorAll(".answer-accordion")
-const addIcon = document.querySelectorAll(".addIcon")
-const closeIcon = document.querySelectorAll(".closeIcon")
 
 btnAccordion.forEach((question, index) => {
     question.addEventListener("click", function () {
-        // console.log(question)
         
+        const addIcon = question.querySelector(".addIcon")
+        const closeIcon = question.querySelector(".closeIcon")
+
         if (answerAccordion[index].classList.contains("max-h-0")) {
             answerAccordion[index].classList.remove("max-h-0")
             answerAccordion[index].classList.add("max-h-screen")
+            addIcon.style.display = "none"
+            closeIcon.style.display = "inline"
         } else {
             answerAccordion[index].classList.remove("max-h-screen")
             answerAccordion[index].classList.add("max-h-0")
+            addIcon.style.display = "inline"
+            closeIcon.style.display = "none"
         }
-
     })
 })
