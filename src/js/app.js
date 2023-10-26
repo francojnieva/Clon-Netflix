2b317552b3c6ee5d87b3906cdc1e2bd8ac502330
francojnieva <franco97nieva@hotmail.com>
2023-10-26 17:23:20 -0300

    :art: change styles to the main title and footer letters. add validation to the login form

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+// Funcionalidad acordeón
+
 const btnAccordion = document.querySelectorAll(".btn-accordion")
 const answerAccordion = document.querySelectorAll(".answer-accordion")
 
@@ -19,4 +21,44 @@ btnAccordion.forEach((question, index) => {
             closeIcon.style.display = "none"
         }
     })
+})
+
+// Validación formulario login
+
+const formLogin = document.getElementById("form-login")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const btnLogin = document.getElementById("btnLogin")
+const warningEmail  = document.getElementById("warning-email")
+const warningPassword  = document.getElementById("warning-password")
+
+email.addEventListener("change", () => {
+    warningEmail.textContent = ""
+})
+
+password.addEventListener("change", () => {
+    warningPassword.textContent = ""
+})
+
+formLogin.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+    if (email.value === "" || password.value === "") {
+        warningEmail.textContent = "Ingresa un email o un número de teléfono válido."
+        warningPassword.textContent = "La contraseña debe tener entre 4 y 60 caracteres."
+        return false
+    }
+
+    const emailRegex = /^\w([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    if (!emailRegex.test(email.value.trim())) {
+        warningEmail.textContent = "Ingresa un email o un número de teléfono válido."
+        return false
+    }
+
+    if (password.value.length < 4  || password.value.length > 60) {
+        warningPassword.textContent = "La contraseña debe tener entre 4 y 60 caracteres."
+        return false
+    }
+    
+    formLogin.submit()
 })
